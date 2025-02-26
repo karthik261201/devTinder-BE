05a35fd77b0e784bf662a7b9450eb6ed2eee4e3b
karthik261201 <karthiknarayanan043@gmail.com>
2025-02-26 17:49:53 +0530

    initialize database

--- src/app.js
+++ src/app.js
@@ -1,26 +1,32 @@
 import express from "express";
 // const express = require("express")
+import { connectDB } from "./config/database.js";
+import User from "./models/user.js";
 
 const app = express()
 
-import { adminAuth, userAuth } from "../middlewares/authMiddleware.js";
+app.post("/signup",async (req,res) => {
+    // creating new instance of User Model
+    const user = new User({
+        firstName: "Karthik",
+        lastName: "Narayanan",
+        emailId: "karthik043@gmail.com",
+        password: "karthik@26"
+    })
 
-app.get("/user/login",(req,res) => {
-    res.send("User Logged In Successfully")
+    try {
+        await user.save()
+        res.send("User Added Successfully!")
+    } catch(err) {
+        res.status(400).send("error in saving: "+err.message)
+    }
 })
 
-app.get("/user/data", userAuth, (req,res) => {
-    res.send("User Data sent")
-})
-
-app.get("/admin/getAllData", adminAuth, (req,res) => {
-    res.send("All Data Sent..")
-})
-
-app.get("/admin/deleteUser", adminAuth, (req,res) => {
-    res.send("Deleted a user..")
-})
-
-app.listen(3000, () => {
-    console.log("Server is successfully running on Port 3000");
-})
+connectDB().then(() => {
+    console.log("DB connected successfully")
+    app.listen(3000, () => {
+        console.log("Server is successfully running on Port 3000");
+    })
+}).catch((err) => {
+    console.error("DB connection failed")
+}) ;
