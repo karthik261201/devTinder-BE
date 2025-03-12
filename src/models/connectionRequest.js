1daee85f18ae4feed882be363a78e16c1f417a91
karthik261201 <karthiknarayanan043@gmail.com>
2025-03-12 21:10:37 +0530

    created fetching connections API

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const ConnectionRequestSchema = new mongoose.Schema({
     fromUserId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User", // reference to user collection
+        ref: "User",
         required: true
     },
     toUserId: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
         required: true
     },
     status: {
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,16 +3,42 @@ import { userAuth } from "../middlewares/auth.js";
 import ConnectionRequest from "../models/connectionRequest.js";
 const userRouter = Router()
 
+const USER_SAFE_DATA = ["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills"]
+
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     try{
         const loggedInUser = req.user
         const connectionRequest = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: "interested"
-        }).populate("fromUserId",["firstName", "lastName", "photoUrl", "age", "gender", "about", "skills"])
+        }).populate("fromUserId",USER_SAFE_DATA)
         res.json({message: "Data fetched successfully",data: connectionRequest})
     }catch(err) {
-        res.status(400).send("ERROR: "+err.message)
+        res.status(400).send("ERROR: "+ err.message)
+    }
+})
+
+userRouter.get("/user/connections", userAuth, async (req,res) => {
+    try{
+        const loggedInUser = req.user
+        const connectionRequest = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId:  loggedInUser._id, status: "accepted"}
+            ]
+        }).populate("toUserId",USER_SAFE_DATA).populate("fromUserId",USER_SAFE_DATA)
+        
+        const data = connectionRequest.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+                return row.toUserId
+            }
+            else {
+                return row.fromUserId
+            }
+        })
+        res.json({ data })
+    }catch(err) {
+        res.status(400).send("ERROR: " + err.message)
     }
 })
 
